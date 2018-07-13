46f59035a14be83b7640c4443c23b623f6a68287
mattstermh93 <mattstermh@hotmail.com>
2018-07-13 10:20:59 -0400

    corrected hover issues on navbar

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,13 @@
 $.get('../../components/header.html', function(response) {
   $("#nav").html(response);
 });
+
+$('body').on('click', 'li', function() {
+      $('li.active').removeClass('active');
+      $(this).addClass('active');
+});
+
+setTimeout(function(){
+  $("#home_link").removeClass("active");
+  $("#prod_link").removeClass("active");
+}, 1);
