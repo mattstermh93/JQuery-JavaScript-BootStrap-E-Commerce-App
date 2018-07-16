255a794104d646af1ec161bc99b2e69cbfa7b04a
mattstermh93 <mattstermh@yahoo.com>
2018-07-15 20:13:44 -0400

    added js function for adding to cart

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,5 @@
+//aesthetics
+
 $.get('../../components/header.html', function(response) {
   $("#nav").html(response);
 });
@@ -11,3 +13,32 @@ setTimeout(function(){
   $("#home_link").removeClass("active");
   $("#prod_link").removeClass("active");
 }, 1);
+
+//Shopping cart functionality below
+
+var cart = []
+var Item = function (name, price, quantity) {
+	this.name = name
+	this.price = price
+	this.quantity = quantity
+	};
+
+
+function addItemtoCart(name, price, count) {
+	for (var i in cart) {
+		if (cart[i].name === name) {
+      cart[i].count += count;
+      return;
+	}
+}
+	var item = new Item(name, price, count);
+	cart.push(item);
+}
+
+function removeItemFromCart(name) {
+  for (var i in cart) {
+    if (cart[i].name === name) {
+
+    }
+  }
+}
