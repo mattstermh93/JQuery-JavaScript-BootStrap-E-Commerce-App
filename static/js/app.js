654e4ff21fe1efdef3555362275e50bff997e7eb
mattstermh93 <mattstermh@hotmail.com>
2018-07-15 22:14:48 -0400

    prep for save and loadcart

--- static/js/app.js
+++ static/js/app.js
@@ -82,5 +82,14 @@ function totalCart() { // return total cost
 }
 
 function listCart() { // return current cart
-  
+  var cartCopy = [];
+  for (var i in cart) {
+    var item = cart[i];
+    var itemCopy = {};
+    for (var p in item) {
+      itemCopy[p] = item[p];
+    }
+    cartCopy.push(itemCopy);
+  }
+  return cartCopy;
 }
